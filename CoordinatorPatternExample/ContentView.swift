f90d1d06d494ca94403f69e80e927c9100d931f8
Yossa Bourne <cybercanonx@gmail.com>
2022-04-03 16:44:41 +0700

    chore: add first commit

---
+++ CoordinatorPatternExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoordinatorPatternExample
+//
+//  Created by Art on 3/4/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoordinatorPatternExample/CoordinatorPatternExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorPatternExampleApp.swift
+//  CoordinatorPatternExample
+//
+//  Created by Art on 3/4/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorPatternExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
